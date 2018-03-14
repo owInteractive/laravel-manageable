e6232963509806458922723c3ebdf43a08de0a16
Fausto Rodrigues <fausto.rodrigues@owinteractive.com>
2018-03-14 10:33:27 -0300

    .

---
+++ src/Events/MediaAttachedTo.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ow\Manageagle\Events;
+
+use Ow\Manageable\Contracts\Manageable;
+use Illuminate\Queue\SerializesModels;
+
+class MediaAttachedTo
+{
+    use SerializesModels;
+
+    public $entity;
+
+    public $media;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  Manageable  $entity
+     * @param  mixed $media
+     * @return void
+     */
+    public function __construct(Manageable $entity, $media)
+    {
+        $this->entity = $entity;
+        $this->media = $media;
+    }
+}
